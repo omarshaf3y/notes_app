eb9bc6d408a89dafddd2c9d52d3af08030453ffb
omar <omarshaf3y99@gmail.com>
2024-03-03 20:09:43 +0200

    add and fix some dependencies

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'custom_appbar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,6 +15,7 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
